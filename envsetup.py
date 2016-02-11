7c42fa92b88ef9350439b9cb2734fb03de776da8
Matyas Selmeci <matyas@cs.wisc.edu>
2016-02-11 20:13:22 +0000

    Add GFAL variables to environment (SOFTWARE-2200)
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/tarball-client/trunk@22466 4e558342-562e-0410-864c-e07659590f8c

--- envsetup.py
+++ envsetup.py
@@ -116,6 +116,8 @@ def write_setup_in_files(dest_dir, dver, basearch):
         text_to_write += _setenv("OSG_LOCATION", "@@OSG_LOCATION@@")
 
         for variable, value in [
+                ("GFAL_CONFIG_DIR", "$OSG_LOCATION/etc/gfal2.d/"),
+                ("GFAL_PLUGIN_DIR", "$OSG_LOCATION/usr/lib64/gfal2-plugins/"),
                 ("GLOBUS_LOCATION", "$OSG_LOCATION/usr"),
                 ("PATH",            "$OSG_LOCATION/usr/bin:$OSG_LOCATION/usr/sbin:$PATH"),
                 ("X509_CERT_DIR",   "$OSG_LOCATION/etc/grid-security/certificates"),
